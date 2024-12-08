a9828dd249ddbd6f99e9b85834ea981e6cad73a4
Gary Vermeulen <60823753+GaryVermeulen@users.noreply.github.com>
2024-12-08 10:28:19 -0800

    Add files via upload

---
+++ strain6/utils/makeFood.py
@@ -0,0 +1,29 @@
+# makeFood.py
+#
+# Generate data (food) for cells.
+#
+
+import random
+import pickle
+
+
+if __name__ == "__main__":
+
+    start = 0
+    # A stop value less than loopCnt will induce starvation
+    stop = 500
+    foodList = []
+
+    for i in range(start, stop):
+        random_int = random.randint(start, stop)
+        foodList.append(random_int)
+
+    for n in foodList:
+        print(n)
+
+    # Save food
+    print('Saving data/food to pickle...')
+    with open("../pickles/dataFood.p", "wb") as f:
+        pickle.dump(foodList, f)
+    f.close()
+    
